06b677030617bb48dcf7b436bf514d619b42fed1
Jack Song <133342301@163.com>
2020-10-05 20:27:50 +1100

    Adding auth to snyk script

---
+++ ci/snyk.sh
@@ -0,0 +1,7 @@
+# Authenticate snyk Cli
+# https://support.snyk.io/hc/en-us/articles/360003812578-CLI-reference
+ snyk auth $SNYK_TOKEN
+
+ # Protect the code from vulnerabilities
+# https://support.snyk.io/hc/en-us/articles/360003812578-CLI-reference
+ snyk protect
