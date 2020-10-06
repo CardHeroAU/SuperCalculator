6c7761466623eae69c55016a19f2050a442062cf
Jack Song <133342301@163.com>
2020-10-07 00:45:15 +1100

    Cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,15 @@
 import React, {useState} from 'react';
-import {SuperSlider, SuperSliderInput} from './components/SuperSlider';
 import {
   Box,
-  Button,
-  ButtonGroup, Chip,
-  createStyles, Divider,
   Fab,
-  Icon, IconButton,
-  InputAdornment, InputBase,
-  MenuItem, Paper,
-  TextField,
-  Theme, Tooltip, Typography
+  Icon,
 } from "@material-ui/core";
-import NavigationIcon from '@material-ui/icons/Navigation';
-import AttachMoney from '@material-ui/icons/AttachMoney';
-import Send from '@material-ui/icons/Send';
-import Remove from '@material-ui/icons/Remove';
-import Add from '@material-ui/icons/Add';
-import {makeStyles} from "@material-ui/core/styles";
 import {SuperInput} from "./components/SuperInput";
 import {SummaryTable} from "./components/SummaryTable";
-import {IncomeTaxTable} from "./components/IncomeTaxTable";
-import {SuperTable} from "./components/SuperTable";
-import {SuperTaxTable} from "./components/SuperTaxTable";
-import {IncomeTable} from "./components/IncomeTable";
-import {TotalTaxTable} from "./components/TotalTaxTable";
-import {SuperCalculation} from "./components/faq/SuperCalculation";
 import {SuperCalculatorProvider} from "./hooks";
-import {SuperTaxation} from "./components/faq/SuperTaxation";
-import {IncomeCalculation} from "./components/faq/IncomeCalculation";
-import {IncomeTaxation} from "./components/faq/IncomeTaxation";
-import {TotalTaxation} from "./components/faq/TotalTaxCalculation";
 import {FAQs} from "./components/faq";
+import {DEFAULT_INCOME, DEFAULT_SACRIFICE_RATE} from "./config";
 
-const defaultIncome = 70000;
 const minimumIncome = 30000;
 const gap = 10000;
 const numberOfGaps = 18;
@@ -43,38 +19,16 @@ for (let i = 1; i < numberOfGaps; i++) {
   incomes.push(income);
 }
 
-const defaultSacrificeRate = 0.10;
-
-// const useStyles = makeStyles((theme: Theme) =>
-//   createStyles({
-//     root: {
-//       '& > *': {
-//         margin: theme.spacing(1),
-//       },
-//     },
-//   }),
-// );
-
-// Create our number formatter.
-const formatter = new Intl.NumberFormat('en-AU', {
-  style: 'currency',
-  currency: 'AUD',
-
-  // These options are needed to round to whole numbers if that's what you want.
-  //minimumFractionDigits: 0,
-  //maximumFractionDigits: 0,
-});
-
 function App() {
 
-  const [totalIncome, setTotalIncome] = useState(defaultIncome);
-  const [sacrificeRate, setSacrificeRate] = useState(defaultSacrificeRate);
+  const [totalIncome, setTotalIncome] = useState(DEFAULT_INCOME);
+  const [sacrificeRate, setSacrificeRate] = useState(DEFAULT_SACRIFICE_RATE);
 
-  const handleSliderChange = (event: any, newValue: number | number[]) => {
-    if (typeof newValue === 'number') {
-      setSacrificeRate(newValue);
+  const updateIncome = (totalIncome: number) => {
+    if (totalIncome >= 0) {
+      setTotalIncome(totalIncome);
     }
-  };
+  }
 
   const updateSacrificeRate = (sacrificeRate: number) => {
     if (sacrificeRate > 0 && sacrificeRate <= 1) {
@@ -87,37 +41,11 @@ function App() {
       <SuperInput sacrificeRate={sacrificeRate}
                   totalIncome={totalIncome}
                   updateSacrificeRate={(sacrificeRate) => updateSacrificeRate(sacrificeRate)}
-                  updateTotalIncome={(totalIncome) => setTotalIncome(totalIncome)}/>
-      <SummaryTable totalIncome={totalIncome} sacrificeRate={sacrificeRate}/>
+                  updateTotalIncome={(totalIncome) => updateIncome(totalIncome)}/>
       <SuperCalculatorProvider totalIncome={totalIncome} sacrificeRate={sacrificeRate}>
-        <FAQs/>
+        <SummaryTable />
+        <FAQs />
       </SuperCalculatorProvider>
-      {/*<form>*/}
-      {/*  <TextField*/}
-      {/*    select*/}
-      {/*    required*/}
-      {/*    id={"totalIncomeSelect"}*/}
-      {/*    label="Total Income"*/}
-      {/*    value={totalIncome}*/}
-      {/*    onChange={(event) => setTotalIncome(parseInt(event.target.value))}*/}
-      {/*    type={"number"}*/}
-      {/*    helperText="Some important text"*/}
-      {/*    variant="outlined"*/}
-      {/*    InputProps={{*/}
-      {/*      startAdornment: (*/}
-      {/*        <InputAdornment position="start">*/}
-      {/*          <AttachMoney/>*/}
-      {/*        </InputAdornment>*/}
-      {/*      ),*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    {incomes.map((income) => (*/}
-      {/*      <MenuItem key={income} value={income}>*/}
-      {/*        {income}*/}
-      {/*      </MenuItem>*/}
-      {/*    ))}*/}
-      {/*  </TextField>*/}
-      {/*</form>*/}
       <Box display="flex">
         <Box m="auto">
           <Fab variant="extended" color="primary" aria-label="add">
--- src/components/SummaryTable/index.tsx
+++ src/components/SummaryTable/index.tsx
@@ -10,23 +10,31 @@ import Paper from '@material-ui/core/Paper';
 import {TotalIncomeRow} from "../TotalIncomeRow";
 import {SuperSacrificeRow} from "../SuperSacrificeRow";
 import {TotalTaxRow} from "../TotalTaxRow";
-import {SacrificeRateProp, TotalIncomeProp} from "../../utils/types";
+import {useSuperCalculator} from "../../hooks";
 
-export const SummaryTable = ({totalIncome, sacrificeRate}: TotalIncomeProp & SacrificeRateProp) => (
-  <TableContainer component={Paper}>
-    <Table aria-label="collapsible table">
-      <TableHead>
-        <TableRow>
-          <TableCell >Summary</TableCell>
-          <TableCell align="right">Original</TableCell>
-          <TableCell align="right">With Sacrifice</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        <TotalIncomeRow totalIncome={totalIncome} />
-        <SuperSacrificeRow totalIncome={totalIncome} sacrificeRate={sacrificeRate} />
-        <TotalTaxRow totalIncome={totalIncome} sacrificeRate={sacrificeRate} />
-      </TableBody>
-    </Table>
-  </TableContainer>
-)
+export const SummaryTable = () => {
+
+  const {
+    totalIncome,
+    sacrificeRate
+  } = useSuperCalculator();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell >Summary</TableCell>
+            <TableCell align="right">Original</TableCell>
+            <TableCell align="right">With Sacrifice</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <TotalIncomeRow totalIncome={totalIncome} />
+          <SuperSacrificeRow totalIncome={totalIncome} sacrificeRate={sacrificeRate} />
+          <TotalTaxRow totalIncome={totalIncome} sacrificeRate={sacrificeRate} />
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
