9f661b83de4f3eb7fefcea130c1481bfef2e4f03
Jack Song <133342301@163.com>
2020-10-16 01:49:52 +1100

    Refactored state into hooks

---
+++ src/hooks/useTotalIncome/index.ts
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import { DEFAULT_INCOME } from '../../utils/constants';
+
+export const useTotalIncome = (): [number, (newIncome: number) => void] => {
+  const [totalIncome, setTotalIncome] = useState(DEFAULT_INCOME);
+
+  const updateTotalIncome = (newIncome: number) => {
+    if (newIncome >= 0) {
+      setTotalIncome(newIncome);
+    } else {
+      console.error('useTotalIncome(): newIncome needs to be >= 0');
+    }
+  };
+
+  return [
+    totalIncome,
+    updateTotalIncome,
+  ];
+};
